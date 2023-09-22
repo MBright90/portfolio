d950294c799e47b3f3edee6176824745f35708e3
mbright90 <michael.bright90@icloud.com>
2023-09-22 10:24:33 +0100

    Initate config files

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: __dirname + '/dist/',
+  },
+  module: {
+    rules: [
+      {   
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: { 
+              presets: ["@babel/env", "@babel/preset-react", "@babel/preset-typescript"]
+            }
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
