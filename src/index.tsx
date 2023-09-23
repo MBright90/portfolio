59b947eabbf2b1bf29034440fda43548af39b181
mbright90 <michael.bright90@icloud.com>
2023-09-23 10:29:28 +0100

    Initiate public & src directories

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+import '@styles/styles.scss'
+
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
+  <React.StrictMode>
+    <div></div>
+  </React.StrictMode>
+)
