f307b424e26b59ed2e48e46b1dd8ecb6d1eee5f6
mbright90 <michael.bright90@icloud.com>
2023-09-27 10:14:24 +0100

    Initiate header directory and files

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import type React from 'react'
+
+import style from './Header.module.scss'
+
+const Header: React.FC = () => {
+  return <header className={style.header}></header>
+}
+
+export default Header
