d8c5fc576539252f577881991dfabe05aa78fa3c
mbright90 <michael.bright90@icloud.com>
2023-09-29 10:09:16 +0100

    Set up basic component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import type React from 'react'
+
+import style from './Footer.module.scss'
+
+const Footer: React.FC = () => {
+  return <footer className={style.footer}></footer>
+}
+
+export default Footer
