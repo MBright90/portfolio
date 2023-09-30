55ceca53cc4a18d28b51fc26bdef7004bf7c6e9e
mbright90 <michael.bright90@icloud.com>
2023-09-30 13:57:39 +0100

    Add further alias resolves

--- webpack.config.js
+++ webpack.config.js
@@ -64,8 +64,10 @@ module.exports = {
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
+      '@contexts': path.resolve(__dirname, 'src/contexts'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
       '@styles': path.resolve(__dirname, 'src/styles'),
-      
+      '@customTypes': path.resolve(__dirname, 'src/types')
     }
   }
 }
