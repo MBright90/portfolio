1efba128c602fe6e89bef80443b91a7817073736
mbright90 <michael.bright90@icloud.com>
2023-10-02 16:23:49 +0100

    Relocate files

--- src/routes/routes/Routes.tsx
+++ src/routes/routes/Routes.tsx
@@ -1,6 +1,6 @@
 import type React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Homepage from '@components/homepage/Homepage'
+import Homepage from '@components/homepage'
 import PageNotFound from '@components/pageNotFound'
 
 const RouteSwitch: React.FC = () => {
