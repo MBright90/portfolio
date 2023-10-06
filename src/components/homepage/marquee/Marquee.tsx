d338834ef72f51d085b7ec26dd103c8071674647
mbright90 <michael.bright90@icloud.com>
2023-10-06 10:32:25 +0100

    Adapt useEffect to trial without width rendering

--- src/components/homepage/marquee/Marquee.tsx
+++ src/components/homepage/marquee/Marquee.tsx
@@ -1,46 +1,46 @@
 import type React from 'react'
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext, useRef } from 'react'
 import { themeContext } from '@contexts/themeContext/ThemeContext'
 
 import style from './Marquee.module.scss'
 
 interface MarqueeProps {
   position: 'top' | 'right' | 'bottom' | 'left'
-  liStrArr: string[]
+  itemArr: string[]
 }
 
-const Marquee: React.FC<MarqueeProps> = ({ position, liStrArr }) => {
+const Marquee: React.FC<MarqueeProps> = ({ position, itemArr }) => {
   const { theme } = useContext(themeContext)
 
-  const wrapperRef = useRef<HTMLDivElement | null>(null)
   const ulRef = useRef<HTMLUListElement | null>(null)
   const clonedUlRef = useRef<HTMLUListElement | null>(null)
 
-  const [listWidth, setListWidth] = useState<number>(0)
+  //   const [listWidth, setListWidth] = useState<number>(10)
 
-  useEffect(() => {
-    if (ulRef.current !== null && clonedUlRef.current !== null) {
-      const listItems = ulRef.current.querySelectorAll('li')
-      let totalWidth = 0
+  //   useLayoutEffect(() => {
+  //     if (ulRef.current !== null && clonedUlRef.current !== null) {
+  //       const listItems = ulRef.current.querySelectorAll('li')
+  //       let totalWidth = 10
 
-      listItems.forEach((item) => {
-        totalWidth += item.getBoundingClientRect().width
-      })
+  //       listItems.forEach((item) => {
+  //         totalWidth += item.getBoundingClientRect().width + 40
+  //       })
 
-      setListWidth(totalWidth)
-    }
-  })
+  //       setListWidth(totalWidth)
+  //     }
+  //   }, [])
 
-  const renderedItems = liStrArr.map((item, index) => <li key={index}>{item}</li>)
+  const renderedItems = itemArr.map((item, index) => (
+    <>
+      <li key={index}>{item}</li>
+      <li className={style.marqueeBullet}>&</li>
+    </>
+  ))
 
   return (
-    <div className={`${style.marquee} ${style[`${position} ${theme}`]}`} ref={wrapperRef}>
-      <ul ref={ulRef} style={{ width: `${listWidth}px` }}>
-        {renderedItems}
-      </ul>
-      <ul ref={clonedUlRef} style={{ width: `${listWidth}px` }}>
-        {renderedItems}
-      </ul>
+    <div className={`${style.marquee} ${style[`${position}`]}  ${theme}`}>
+      <ul ref={ulRef}>{renderedItems}</ul>
+      <ul ref={clonedUlRef}>{renderedItems}</ul>
     </div>
   )
 }
