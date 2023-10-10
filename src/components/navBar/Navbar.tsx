52c5bebd3ec0646b25abdcb207f7b2b74291731f
mbright90 <michael.bright90@icloud.com>
2023-10-10 14:46:04 +0100

    Add navbar toggle element

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -1,26 +1,38 @@
 import type React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import style from './Navbar.module.scss'
 
 const Navbar: React.FC = () => {
+  const [dropActive, setDropActive] = useState<boolean>(false)
+
+  const toggleNavMenu = (): void => {
+    setDropActive(!dropActive)
+  }
+
   return (
     <nav className={style.navbar}>
-      <Link to="/" className={style.navbarLink}>
-        HOME
-      </Link>
-      <Link to="/" className={style.navbarLink}>
-        PROJECTS
-      </Link>
-      <Link to="/" className={style.navbarLink}>
-        ABOUT ME
-      </Link>
-      <Link to="/" className={style.navbarLink}>
-        SOMETHING ELSE
-      </Link>
-      <Link to="/" className={style.navbarLink}>
-        CONTACT
-      </Link>
+      <div className={style.navToggle} role="button" onClick={toggleNavMenu}>
+        Tog
+      </div>
+      <ul className={style.navLinkList}>
+        <Link to="/" className={style.navbarLink}>
+          HOME
+        </Link>
+        <Link to="/" className={style.navbarLink}>
+          PROJECTS
+        </Link>
+        <Link to="/" className={style.navbarLink}>
+          ABOUT ME
+        </Link>
+        <Link to="/" className={style.navbarLink}>
+          SOMETHING ELSE
+        </Link>
+        <Link to="/" className={style.navbarLink}>
+          CONTACT
+        </Link>
+      </ul>
     </nav>
   )
 }
