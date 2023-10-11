17140bf2e4d10f4a995d39d993a185f2aff532b7
mbright90 <michael.bright90@icloud.com>
2023-10-11 10:04:19 +0100

    Initiate Seenit project component

---
+++ src/components/projects/seenit/Seenit.tsx
@@ -0,0 +1,18 @@
+import type React from 'react'
+
+import style from './Seenit.module.scss'
+
+const Seenit: React.FC = () => {
+  return (
+    <div className={style.projectWrapper}>
+      <div>Title stuff</div>
+      <div>Mobile example</div>
+      <div>Details one</div>
+      <div>Desktop example</div>
+      <div>Details two</div>
+      <div>Links</div>
+    </div>
+  )
+}
+
+export default Seenit
