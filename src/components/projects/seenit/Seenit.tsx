65e7a209fc8b79a90529d43ac435a28a5907406e
mbright90 <michael.bright90@icloud.com>
2023-10-13 10:20:41 +0100

    Add classNames to corresponding elements

--- src/components/projects/seenit/Seenit.tsx
+++ src/components/projects/seenit/Seenit.tsx
@@ -8,11 +8,16 @@ const Seenit: React.FC = () => {
       <div className={style.titleWrapper}>
         <h1 className={style.projectTitle}>SeenIt</h1>
       </div>
-      <div>Mobile example</div>
-      <div>Details one</div>
-      <div>Desktop example</div>
-      <div>Details two</div>
-      <div>Links</div>
+
+      <div className={style.mobileExampleWrapper}>Mobile example</div>
+
+      <div className={`${style.desktopExample} ${style.desktopOne}`}>Desktop example one</div>
+      <div className={`${style.desktopExample} ${style.desktopTwo}`}>Desktop example two</div>
+
+      <div className={`${style.projectDetails} ${style.detailsOne}`}>Details one</div>
+      <div className={`${style.projectDetails} ${style.detailsTwo}`}>Details two</div>
+
+      <div className={style.projectLinks}>Links</div>
     </div>
   )
 }
