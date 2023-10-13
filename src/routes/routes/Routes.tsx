09ec618eefc8a49fbc332eea33c0337703d295b0
mbright90 <michael.bright90@icloud.com>
2023-10-13 10:20:21 +0100

    Add temporary home route

--- src/routes/routes/Routes.tsx
+++ src/routes/routes/Routes.tsx
@@ -7,7 +7,8 @@ import ProjectsPage from '@components/projectsPage/ProjectsPage'
 const RouteSwitch: React.FC = () => {
   return (
     <Routes>
-      <Route key={location.pathname} path="/" element={<Homepage />} />
+      <Route key={location.pathname} path="/" element={<ProjectsPage />} />
+      <Route key={location.pathname} path="/home" element={<Homepage />} />
       <Route key={location.pathname} path="/projects" element={<ProjectsPage />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
