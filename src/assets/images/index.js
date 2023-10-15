8e71a28f8f2f739f898c5b85391bb69da2b2db64
mbright90 <michael.bright90@icloud.com>
2023-10-15 16:47:49 +0100

    Update index to export added image files

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,7 @@
 import logo from './logo.png'
+import seenitLogin from './seenit-login-example.png'
+import seenitNewPost from './seenit-new-post-example.png'
+import seenitProfile from './seenit-profile-example.png'
+import seenitResponsive from './seenit-responsive-example.png'
 
-export { logo }
+export { logo, seenitLogin, seenitNewPost, seenitProfile, seenitResponsive }
