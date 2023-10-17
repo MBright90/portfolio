0bcf1bb7c87e06770f74497c31d79152a37ccabd
mbright90 <michael.bright90@icloud.com>
2023-10-17 16:15:21 +0100

    Reset site routes

--- src/routes/routes/Routes.tsx
+++ src/routes/routes/Routes.tsx
@@ -2,14 +2,11 @@ import type React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Homepage from '@components/homepage'
 import PageNotFound from '@components/pageNotFound'
-import ProjectsPage from '@components/projectsPage/ProjectsPage'
 
 const RouteSwitch: React.FC = () => {
   return (
     <Routes>
-      <Route key={location.pathname} path="/" element={<ProjectsPage />} />
-      <Route key={location.pathname} path="/home" element={<Homepage />} />
-      <Route key={location.pathname} path="/projects" element={<ProjectsPage />} />
+      <Route key={location.pathname} path="/" element={<Homepage />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   )
