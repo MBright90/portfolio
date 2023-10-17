f9595e008b90277779a6442b4d4f5afca7bd1545
mbright90 <michael.bright90@icloud.com>
2023-10-17 16:04:15 +0100

    Remove unnecessary images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,1 @@
-import logo from './logo.png'
-import seenitLogin from './seenit-login-example.png'
-import seenitNewPost from './seenit-new-post-example.png'
-import seenitProfile from './seenit-profile-example.png'
-import seenitResponsive from './seenit-responsive-example.png'
-
-export { logo, seenitLogin, seenitNewPost, seenitProfile, seenitResponsive }
+export {}
