fddb481fea038bf943f193437228136378565123
mbright90 <michael.bright90@icloud.com>
2023-10-17 17:31:23 +0100

    Simplify app providers

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,22 +1,16 @@
 import type React from 'react'
-import { useContext } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import ThemeContextProvider, { themeContext } from '@contexts/themeContext'
 import RouteSwitch from '@routes/routes'
 
 import style from './App.module.scss'
 
 const App: React.FC = () => {
-  const { theme } = useContext(themeContext)
-
   return (
-    <ThemeContextProvider>
-      <BrowserRouter>
-        <div className={`${style.appWrapper} ${theme}`}>
-          <RouteSwitch />
-        </div>
-      </BrowserRouter>
-    </ThemeContextProvider>
+    <BrowserRouter>
+      <div className={style.appWrapper}>
+        <RouteSwitch />
+      </div>
+    </BrowserRouter>
   )
 }
 
