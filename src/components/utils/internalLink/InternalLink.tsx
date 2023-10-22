c48b3e19788543ed0af5dbd13ec0176332ac600b
mbright90 <michael.bright90@icloud.com>
2023-10-22 10:33:12 +0100

    Alter a element to button

--- src/components/utils/internalLink/InternalLink.tsx
+++ src/components/utils/internalLink/InternalLink.tsx
@@ -1,18 +1,19 @@
 import type React from 'react'
-import { Link } from 'react-router-dom'
 
 import style from './InternalLink.module.scss'
 
 interface InternalLinkInterface {
   linkText: string
+  route: string
 }
 
-const InternalLink: React.FC<InternalLinkInterface> = ({ linkText }) => {
-  return (
-    <Link className={style.internalLink} to="/">
-      {linkText}
-    </Link>
-  )
+const InternalLink: React.FC<InternalLinkInterface> = ({ linkText, route }) => {
+  const transitionToPage = (route: string) => {
+    // Add css transition here
+    // COLOR COVERS SCREEN BEFORE TRANSITIONING TO NEW PAGE
+  }
+
+  return <button className={style.internalLink}>{linkText}</button>
 }
 
 export default InternalLink
