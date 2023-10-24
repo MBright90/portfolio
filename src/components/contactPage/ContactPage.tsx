0aeece7c69b51a46895e83ce78dc093cf6d83d46
mbright90 <michael.bright90@icloud.com>
2023-10-24 17:10:47 +0100

    Input initial ContactPage styles and structure

--- src/components/contactPage/ContactPage.tsx
+++ src/components/contactPage/ContactPage.tsx
@@ -1,10 +1,24 @@
 import type React from 'react'
+import { createPortal } from 'react-dom'
+import TransitionIn from '@components/utils/transitionIn'
+import useTransition, { type TransitionHookReturnType } from '@hooks/useTransition'
 
 import style from './ContactPage.module.scss'
 
 const ContactPage: React.FC = () => {
+  const { transitionBool, removeTransition }: TransitionHookReturnType = useTransition()
+
+  const transitionIn = (): React.ReactNode | null => {
+    if (transitionBool as boolean) {
+      removeTransition()
+      return createPortal(<TransitionIn />, document.querySelector('#root')!)
+    }
+    return null
+  }
+
   return (
     <main className={style.contactPageWrapper}>
+      {transitionIn()}
       <form></form>
     </main>
   )
