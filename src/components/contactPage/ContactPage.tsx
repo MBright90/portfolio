e7c9c95e158544b4dad8c34e6f48f3bd2ead249f
mbright90 <michael.bright90@icloud.com>
2023-10-24 15:22:08 +0100

    Add contactPage directory

---
+++ src/components/contactPage/ContactPage.tsx
@@ -0,0 +1,13 @@
+import type React from 'react'
+
+import style from './ContactPage.module.scss'
+
+const ContactPage: React.FC = () => {
+  return (
+    <main className={style.contactPageWrapper}>
+      <form></form>
+    </main>
+  )
+}
+
+export default ContactPage
