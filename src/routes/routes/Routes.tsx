b4db3d58c1ec2c6304ae7b733271b9679327a62f
mbright90 <michael.bright90@icloud.com>
2023-10-24 17:05:43 +0100

    Implement ContactPage element route

--- src/routes/routes/Routes.tsx
+++ src/routes/routes/Routes.tsx
@@ -1,6 +1,6 @@
 import type React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import ContactPage from '@components/contactPage/ContactPage'
+import ContactPage from '@components/contactPage'
 import Homepage from '@components/homepage'
 import PageNotFound from '@components/pageNotFound'
 
