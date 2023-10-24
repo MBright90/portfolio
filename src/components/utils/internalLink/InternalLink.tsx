4f11c2d52c57bc12076f58cb5d5ff404c7e5f17d
mbright90 <michael.bright90@icloud.com>
2023-10-24 13:14:17 +0100

    Add linkWrapper for transition

--- src/components/utils/internalLink/InternalLink.tsx
+++ src/components/utils/internalLink/InternalLink.tsx
@@ -1,4 +1,6 @@
 import type React from 'react'
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import style from './InternalLink.module.scss'
 
@@ -8,12 +10,28 @@ interface InternalLinkInterface {
 }
 
 const InternalLink: React.FC<InternalLinkInterface> = ({ linkText, route }) => {
-  const transitionToPage = (route: string) => {
-    // Add css transition here
-    // COLOR COVERS SCREEN BEFORE TRANSITIONING TO NEW PAGE
+  const navigate = useNavigate()
+  const transitionOutRef = useRef<HTMLDivElement | null>(null)
+
+  const followRoute = (): void => {
+    transitionOutRef.current?.removeEventListener('animationend', followRoute)
+    navigate(route)
+    transitionOutRef.current?.classList.remove(style.active)
   }
 
-  return <button className={style.internalLink}>{linkText}</button>
+  const transitionToPage = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    transitionOutRef.current?.addEventListener('animationend', followRoute)
+    transitionOutRef.current?.classList.add(style.active)
+  }
+
+  return (
+    <div className={style.linkWrapper}>
+      <button className={style.internalLink} onClick={transitionToPage}>
+        {linkText}
+      </button>
+      <div className={style.transitionOut} ref={transitionOutRef}></div>
+    </div>
+  )
 }
 
 export default InternalLink
