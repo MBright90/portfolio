05fdfcbdedcf8f2eb2d21f610f479dfe670c2ae0
mbright90 <michael.bright90@icloud.com>
2023-10-26 10:23:29 +0100

    Refine logic following animation end

--- src/components/utils/transitionIn/TransitionIn.tsx
+++ src/components/utils/transitionIn/TransitionIn.tsx
@@ -1,16 +1,21 @@
 import type React from 'react'
-import { useEffect, useRef } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { createPortal } from 'react-dom'
+import { transitionContext } from '@contexts/transitionContext/transitionContext'
 
 import style from './TransitionIn.module.scss'
 
 const TransitionIn: React.FC = () => {
   const transitionDivRef = useRef<HTMLDivElement | null>(null)
+  const { setTransitionStatus } = useContext(transitionContext)
 
   useEffect(() => {
     if (transitionDivRef.current != null) {
       transitionDivRef.current.addEventListener('animationend', () => {
-        if (transitionDivRef.current != null) transitionDivRef.current.style.display = 'none'
+        if (transitionDivRef.current != null) {
+          transitionDivRef.current.style.display = 'none'
+          setTransitionStatus(false)
+        }
       })
     }
   }, [transitionDivRef.current])
