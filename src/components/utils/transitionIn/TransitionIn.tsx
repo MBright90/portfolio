8863982f261eae6c51563213144a97c6d5407c44
mbright90 <michael.bright90@icloud.com>
2023-10-26 09:56:18 +0100

    Add useEffect to remove transition on completion

--- src/components/utils/transitionIn/TransitionIn.tsx
+++ src/components/utils/transitionIn/TransitionIn.tsx
@@ -1,11 +1,23 @@
 import type React from 'react'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { createPortal } from 'react-dom'
 
 import style from './TransitionIn.module.scss'
 
 const TransitionIn: React.FC = () => {
   const transitionDivRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (transitionDivRef.current != null) {
+      transitionDivRef.current.addEventListener('animationend', () => {
+        if (transitionDivRef.current != null) transitionDivRef.current.style.display = 'none'
+      })
+    }
+  }, [transitionDivRef.current])
+
+  transitionDivRef.current?.addEventListener('transitionend', () => {
+    if (transitionDivRef.current != null) transitionDivRef.current.style.display = 'none'
+  })
 
   return createPortal(
     <div className={style.transitionIn} ref={transitionDivRef}></div>,
