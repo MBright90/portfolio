d0595df78c652544834e0d8fd3fcaaa037e848a6
mbright90 <michael.bright90@icloud.com>
2023-10-28 16:53:50 +0100

    Add further structuring and styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,11 @@ import type React from 'react'
 import style from './Header.module.scss'
 
 const Header: React.FC = () => {
-  return <header className={style.header}> Header </header>
+  return (
+    <header className={style.header}>
+      <button className={style.headerBtn}></button>
+    </header>
+  )
 }
 
 export default Header
