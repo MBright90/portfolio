efbfcc54237c2ecdeba2038eef7fc2ccd1532c3b
mbright90 <michael.bright90@icloud.com>
2023-10-29 16:46:55 +0000

    Add HeaderLink components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,16 @@
 import type React from 'react'
 
+import HeaderLink from './headerLink/HeaderLink'
+
 import style from './Header.module.scss'
 
 const Header: React.FC = () => {
   return (
     <header className={style.header}>
-      <button className={style.headerBtn}></button>
+      <HeaderLink linkText={'HOME'} route="/" />
+      <HeaderLink linkText={'PROJECTS'} route="/" />
+      <HeaderLink linkText={'ABOUT ME'} route="/" />
+      <HeaderLink linkText={'CONTACT'} route="/contact" />
     </header>
   )
 }
