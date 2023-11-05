0086140726790b85239c2a6d86a5207199a01f5c
mbright90 <michael.bright90@icloud.com>
2023-11-05 11:22:48 +0000

    initiate ContactForm directory

---
+++ src/components/contactPage/contactForm/ContactForm.tsx
@@ -0,0 +1,9 @@
+import type React from 'react'
+
+import style from './ContactForm.module.scss'
+
+const ContactForm: React.FC = () => {
+  return <form className={style.formContainer}></form>
+}
+
+export default ContactForm
